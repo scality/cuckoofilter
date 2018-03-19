3b40eeebe3df97235dddd531cf37345cf97427e2
Matteo Collina <hello@matteocollina.com>
2018-03-19 12:45:47 +0100

    Added some basic benchmarks

---
+++ benchmarks/basic.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const benchmark = require('benchmark')
+const suite = benchmark.Suite()
+const Cuckoo = require('..')
+const CuckooFilter = require('cuckoo-filter').CuckooFilter
+const BloomFilter = require('bloomfilter').BloomFilter
+
+const cuckoo = new Cuckoo()
+const cuckoofilter = new CuckooFilter(200, 4, 2)
+
+const bloom = new BloomFilter(
+  32 * 256,
+  16
+)
+
+cuckoo.add('hello world')
+cuckoofilter.add('hello world')
+bloom.add('hello world')
+
+suite.add('cuckoo add', function () {
+  cuckoo.add('hello world' + Math.random())
+})
+
+suite.add('cuckoo contain', function () {
+  cuckoo.contain('hello world')
+})
+
+suite.add('cuckoofilter add', function () {
+  cuckoofilter.add('hello world' + Math.random())
+})
+
+suite.add('cuckoofilter contains', function () {
+  cuckoofilter.contains('hello world')
+})
+
+suite.add('bloomfilter add', function () {
+  bloom.add('hello world' + Math.random())
+})
+
+suite.add('bloomfilter contains', function () {
+  bloom.test('hello world')
+})
+
+suite
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {})
+  .run()
